e638bf998d545f0cbdfac07163ebd8ed85327b9b
Kymemia <kimemiachristopherr@gmail.com>
2024-01-04 20:08:29 +0300

    checking if script is being run as the main program

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+
+    from add_0 import add
 a = 1
 b = 2
 sum = add(a, b)
 
-print(f"{a:d} + {b:d} = {sum:d}")
+print("{} + {} = {}".format(a, b, add(a, b)))
 
