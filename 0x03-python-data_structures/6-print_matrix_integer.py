d647486cc896869c4036f4e8e0f77f4dd59c966c
Kymemia <kimemiachristopherr@gmail.com>
2024-01-09 20:44:44 +0300

    tuple addition

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,8 @@ def print_matrix_integer(matrix=[[]]):
         col_idx = 0
         while col_idx < len(matrix[row_idx]):
             col = matrix[row_idx][col_idx]
-            print('{:d}'.format(col), end = ' ' if col_idx < len(matrix[row_idx]) - 1 else '')
+            print('{:d}'.format(col),
+                    end=' ' if col_idx < len(matrix[row_idx]) - 1 else '')
             col_idx += 1
         print()
         row_idx += 1
