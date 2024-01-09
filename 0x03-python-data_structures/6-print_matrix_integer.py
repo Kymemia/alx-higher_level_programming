88d3e2a1b8d213db2592958e7eb1f678b7321c76
Kymemia <kimemiachristopherr@gmail.com>
2024-01-09 16:11:44 +0300

    return of the matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for col in row:
-            print(f'{col:d}', end=' ' if col != row[-1] else  '')
+    row_idx = 0
+    while row_idx < len(matrix):
+        col_idx = 0
+        while col_idx < len(matrix[row_idx]):
+            col = matrix[row_idx][col_idx]
+            print('{:d}'.format(col), end = ' ' if col_idx < len(matrix[row_idx]) - 1 else '')
+            col_idx += 1
         print()
+        row_idx += 1
