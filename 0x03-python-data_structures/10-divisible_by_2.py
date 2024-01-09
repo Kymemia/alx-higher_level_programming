9c1ad2540ee103bb9deb89e4b6efe7549a3c28e9
Kymemia <kimemiachristopherr@gmail.com>
2024-01-09 22:27:11 +0300

    multiple of dos

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_lisy = []
+    for x in range(len(my_list)):
+        if my_list[x] % 2 == 0:
+            new_lisy.append(True)
+        else:
+            new_lisy.append(False)
+        return new_lisy
