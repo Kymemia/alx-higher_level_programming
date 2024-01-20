0598b5934043c44cdad0b9795b25bf3da2a496b5
Kymemia <kimemiachristopherr@gmail.com>
2024-01-20 17:04:45 +0300

    unique fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    return sum(map(lambda i: i, my_list))
+    unique = set()
+    u_sum = 0
+
+    for i in my_list:
+        if i not in unique:
+            unique.add(i)
+            u_sum += i
+    return u_sum
