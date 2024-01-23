d192f4f9b6f5d21c7430593163d226a65b711ace
Kymemia <kimemiachristopherr@gmail.com>
2024-01-23 20:49:43 +0300

    class that defines a square(2)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""class, Square, that defines a Square"""
+
+
+class Square:
+    """square definition"""
+    def __init__(self, size):
+        """definition of private instance attribute named, size"""
+        self.__size = size
