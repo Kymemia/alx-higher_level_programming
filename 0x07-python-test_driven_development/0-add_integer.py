96e63e36e66fb20a40c3a2d87c397b7ed7348686
Kymemia <kimemiachristopherr@gmail.com>
2024-02-03 01:57:39 +0300

    comment fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-"""adds 2 integers"""
+"""Module that adds 2 numbers
+
+This is a module that adds two numbers.
+First, it casts integers if they are float.
+"""
 
 
 def add_integer(a, b=98):
-    """returns the sum of int a and int b"""
+    """returns the sum of int a and int b
+
+    Args:
+        a (:obj: 'int, float'): The first number
+        b (:obj: int, float', optional): The second number
+
+    Returns:
+        int: sum as an integer
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError('a must be an integer')
     if not isinstance(b, int) and not isinstance(b, float):
@@ -11,7 +23,14 @@ def add_integer(a, b=98):
     return int(a) + int(b)
 
 def int_conversion(num):
-    """convert a float to int"""
+    """convert a float to int
+
+    Args:
+        num(:obj: 'int, float'): Number to cast
+
+    Returns:
+        int: number to be cast
+    """
     if type(num) is float:
         num = int(num)
         return num
