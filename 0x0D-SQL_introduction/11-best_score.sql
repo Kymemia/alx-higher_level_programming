9d24ce9a6d5440bf23eb28f11846ad5b2cb82711
Kymemia <kimemiachristopherr@gmail.com>
2024-03-13 01:26:50 +0300

    12

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all records with a score >= 10 in second_table of hbtn_0c_0
-SELECT * FROM second_table WHERE score >= 10;
+SELECT * FROM second_table WHERE score >= 10 ORDER BY score DESC;
