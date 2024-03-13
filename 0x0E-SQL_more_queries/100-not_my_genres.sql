65b00d0158c64b004d80c472b040d9789dacdcb9
Kymemia <kimemiachristopherr@gmail.com>
2024-03-14 01:12:59 +0300

    100.0

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,8 +1,9 @@
 -- uses the hbtn_0d_tvshows DB to list all genres not linked to the show, Dexter
 SELECT name FROM tv_genres
-WHERE name NOT IN (
-	SELECT name FROM tv_genres
-	LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre.id
-	LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-	WHERE tv_shows.title = 'Dexter')
-GROUP BY name GROUP BY name ASC;
+WHERE id NOT IN (
+	SELECT DISTINCT tv__show_genre_id
+	FROM tv_show_genres
+	JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY  name ASC;
