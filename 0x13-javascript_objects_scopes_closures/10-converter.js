f1a81ad26e00fb6c000878c4aeedd33eb5fbcd36
Kymemia <kimemiachristopherr@gmail.com>
2024-04-10 18:05:28 +0300

    converter -> variable declaration fix

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
 exports.converter = function (base) {
   return function BaseConvert (number) {
-    if (number === 0) {
-      return 0;
-    } else {
-      let fp = '';
-      while (number > 0) {
-        const remainder = number % base;
-        if (base === 16 && remainder > 9) {
-          fp = String.fromCharCode(remainder + 55) + fp;
-        } else {
-          fp = remainder + fp;
-        }
-        number = Math.floor(number / base);
-      }
-      return fp;
-    }
+    return (function convert(numm, fp) {
+      return numm === 0
+        ? fp
+        : convert(Math.floor(numm / base), (base === 16 && numm % base > 9)
+          ? String.fromCharCode(numm % base + 55) + fp
+          : numm % base + fp);
+    })(number, '');
   };
 };
