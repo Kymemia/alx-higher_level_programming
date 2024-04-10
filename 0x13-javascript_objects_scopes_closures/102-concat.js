e645eba42978aa91d9b3a95935adfea369c15791
Kymemia <kimemiachristopherr@gmail.com>
2024-04-10 21:33:58 +0300

    cat

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,16 +5,19 @@ const [fileA, fileB, fileC] = process.argv;
 
 fs.readFile(fileA, 'utf8', (err, data1) => {
   if (err) {
+    console.error(err);
     return;
   }
   fs.readFile(fileB, 'utf8', (err, data2) => {
     if (err) {
+      console.error(err);
       return;
     }
     const result = data1 + data2;
 
     fs.writeFile(fileC, result, 'utf8', err=> {
       if (err) {
+        console.error(err);
         return;
       }
     });
