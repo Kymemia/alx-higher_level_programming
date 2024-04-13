0ae4eba4d586a69ad1dc42c38b86a80c9f578dd9
Kymemia <kimemiachristopherr@gmail.com>
2024-04-14 01:51:45 +0300

    2**

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,16 @@ import sys
 
 def myfilter_states(username, password, database, state_name):
     """connects to the MySQL database and extracts username,
-    password, database, and state_name"""
+    password, database, and state_name
+    Args:
+        username (str): MySQL username
+        password (str): MySQL password
+        database (str): Database name
+        state_name (str): The name of the state to be searched
+
+    Returns:
+        None
+    """
     connection = MySQLdb.connect(host='localhost',
                                  user=username,
                                  passwd=password,
@@ -18,8 +27,8 @@ def myfilter_states(username, password, database, state_name):
 
     cursor = connection.cursor()
 
-    sql_query = "SELECT DISTINCT * FROM states
-    WHERE name = '{}' ORDER BY id ASC".format(state_name)
+    sql_query = ("SELECT DISTINCT * FROM states "
+                    "WHERE name = '{}' ORDER BY id ASC".format(state_name))
     cursor.execute(sql_query)
 
     states = cursor.fetchall()
