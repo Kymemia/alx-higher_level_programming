53d150d730dbcafc56091b90a42e4b9240bf9ec5
Kymemia <kimemiachristopherr@gmail.com>
2024-04-19 03:16:46 +0300

    7-n

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,36 +1,26 @@
 #!/usr/bin/python3
 
-"""script lists all State objects from the DB, hbtn_0e_6_usa"""
+"""This is a script that lists all state objects from DB, hbtn_0e_6_usa"""
 
-from sqlalchemy import create_engine
 import sys
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
 from model_state import Base, State
 
 
-def list_states(username, password, database_name):
-    """list all State Objects from the database"""
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(username, password, database_name))
-    Base.metadata.create_all(engine)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:@localhost:3306/{}'.format(
+                            sys.argv[1], sys.argv[2], sys.argv[3],
+                            pool_pre_ping=True)
 
     Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
+
     session = Session()
 
-    states = session.query(state).order_by(State.id).all()
+    states = session.query(State).order_by(State_id).all()
+
     for state in states:
         print("{}: {}".format(state.id, state.name))
 
     session.close()
-
-
-if __name__ == "__main__":
-    """checks the number of args"""
-    if len(sys.argv) != 4:
-        sys.exit(1)
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    list_states(username, password, database_name)
