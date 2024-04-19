e2a53a3c498a118762cb4f778851b9c1ec90e6a2
Kymemia <kimemiachristopherr@gmail.com>
2024-04-19 03:27:05 +0300

    7**

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,7 +8,15 @@ from sqlalchemy import create_engine
 from model_state import Base, State
 
 
-if __name__ == "__main__":
+def list_states(username, password, database_name):
+    """lists all the state objects from hbtn_0e_6_usa.
+    Args:
+        username (str): Username for connecting to the MySQL server
+        password (str): The password for connecting to the MySQL server
+        database_name (str): The name of the database
+
+    Returns:
+        None"""
     engine = create_engine('mysql+mysqldb://{}:@localhost:3306/{}'.format(
                             sys.argv[1], sys.argv[2], sys.argv[3],
                             pool_pre_ping=True)
@@ -26,3 +34,14 @@ if __name__ == "__main__":
         print("{}: {}".format(state.id, state.name))
 
     session.close()
+
+if __name == "__main__":
+    """condition to check accurate number of arguments"""
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    list_states(username, password, database_name)
