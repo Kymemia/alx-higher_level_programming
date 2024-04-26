20539812e26290c8383251c53764d98a77d61ef9
Kymemia <kimemiachristopherr@gmail.com>
2024-04-26 04:51:57 +0300

    back to basics

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-[ $# -lt 2 ] && echo "Usage: $0 <URL> <filename>" && exit 1 curl -s "$1" -d "@2" -X POST -H "Content-Type: application/json"
+curl -s "$1" -d "@2" -X POST -H "Content-Type: application/json"
