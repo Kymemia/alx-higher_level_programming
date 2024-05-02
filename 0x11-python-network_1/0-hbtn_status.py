638a4dc43fcfc72af760104eece9bf015a902b89
Kymemia <kimemiachristopherr@gmail.com>
2024-05-02 14:02:30 +0300

    0..

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """This is a script that fetches a URL"""
 
-import urllib.request
+if __name__ == "__main__":
+    import urllib.request
 
-url = "https://alx-intranet.hbtn.io/status"
+    url = "https://alx-intranet.hbtn.io/status"
 
-"""sends a get request to the URL, reads the response body, and decodes it"""
-with urllib.request.urlopen(url) as response:
-    body = response.read()
+    """sends a get request to the URL, reads the response body, and decodes it"""
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
 
-print("\t- Body response:")
-print("\t\t- type:", type(body))
-print("\t\t- content:", body)
-print("\t\t- utf8 content:", body.decode('utf-8'))
+        print("\t- Body response:")
+        print("\t\t- type:", type(body))
+        print("\t\t- content:", body)
+        print("\t\t- utf8 content:", body.decode('utf-8'))
