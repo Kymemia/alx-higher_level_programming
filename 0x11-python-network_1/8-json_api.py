46331238013ce316c40cb14fe03e00cee33c172b
Kymemia <kimemiachristopherr@gmail.com>
2024-05-05 19:50:20 +0300

    search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""This script takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    url = "http://0.0.0.0:5000/search_user"
+    try:
+        content = requests.post(url, data={'q': sys.argv[1]})
+        jsonData = content.json()
+        print(f"[{jsonData.get('id')}] {jsonData.get('name')}"
+              if jsonData else "No result")
+    except (ValueError, IndexError):
+        print("No result")
