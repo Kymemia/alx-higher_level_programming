56da6f624fc67e0aa4d667b0f27f953b2754f293
Kymemia <kimemiachristopherr@gmail.com>
2024-05-23 02:17:25 +0300

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filepath = process.argv[3];
+const fileStream = fs.createWriteStream(filepath);
+
+request.get(url)
+  .on('error', err => console.error(err))
+  .pipe(fileStream)
+  .on('finish', () => console.log('success'))
+  .on('error', err => console.log(err));
